a843f9fc29dfdc265552b9bdb57280a12f5d64f0
ver217 <lhx0217@gmail.com>
2022-04-02 15:36:11 +0800

    add release script

---
+++ release.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+'use strict';
+
+const { ArgumentParser } = require('argparse')
+const fs = require('fs')
+const path = require('path')
+const sidebar = require('./sidebars')
+
+
+function updateDocs(locale, version) {
+    const docsRoot = path.join('i18n', locale, 'docusaurus-plugin-content-docs')
+    fs.cpSync(path.join(docsRoot, 'current'), path.join(docsRoot, `version-${version}`), { recursive: true })
+    fs.cpSync(path.join(docsRoot, 'current.json'), path.join(docsRoot, `version-${version}.json`))
+}
+
+function updateSidebar(version) {
+    const json = JSON.stringify(sidebar, null, 2)
+    fs.writeFileSync(`versioned_sidebars/version-${version}-sidebars.json`, json)
+}
+
+function updateSymlink(version) {
+    process.chdir('versioned_docs')
+    fs.symlinkSync(`../i18n/en/docusaurus-plugin-content-docs/version-${version}`, `version-${version}`)
+}
+
+function main() {
+    const parser = new ArgumentParser({
+        description: 'A script to release a new version.'
+    })
+    parser.add_argument('version', { help: 'The version you want to release. Do start the version with a "v" for consistency. For example, v0.1.0' })
+    const args = parser.parse_args()
+    updateSidebar(args.version)
+    updateDocs('en', args.version)
+    updateDocs('zh-Hans', args.version)
+    updateSymlink(args.version)
+}
+
+main()
