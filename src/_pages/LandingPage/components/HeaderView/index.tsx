d437a12e9d4a833defe4ebbb4daf8fe5e2647a4c
Sandro Groganz <groganz@users.noreply.github.com>
2023-01-03 04:20:07 +0100

    Changed home page (translation missing) (#142)
    * Changed home page (translation missing)
    
    * feature: home page chinese translation
    
    * Added missing Slack URL https://github.com/hpcaitech/ColossalAI-Documentation/pull/142#discussion_r1059303664
    
    * Added link to examples #142
    
    * Added link to examples #142
    
    Co-authored-by: Minghao Huang <huangminghao@luchentech.com>

--- src/_pages/LandingPage/components/HeaderView/index.tsx
+++ src/_pages/LandingPage/components/HeaderView/index.tsx
@@ -2,13 +2,12 @@ import { translate } from '@docusaurus/Translate';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import React, { useState } from 'react';
+import SlackButton from '../../../../components/buttons/SlackButton';
 import GithubButton from '../../../../components/buttons/GithubButton';
 import PrimaryButton from '../../../../components/buttons/PrimaryButton';
 import Spacer from '../../../../components/other/Spacer';
 import { useWindowSize } from '../../../../hooks/useWindowSize';
 import { shuffle } from '../../../../utils';
-import HeaderTyper from './components/HeaderTyper';
-import Rocket from './components/Rocket';
 import styles from './styles.module.css';
 import { trans } from '../../../../i18n';
 
@@ -21,28 +20,18 @@ const HeaderView: React.FC<Props> = ({ getStartedRef }) => {
     shuffle(trans('landing.typer', i18n))
   );
 
-  const getStartedUrl = useBaseUrl('/download')
+  const getStartedUrl = useBaseUrl('/docs/get_started/installation')
 
   return (
     <div
-      className={styles.Container}
-      style={{ height: windowHeight > 800 ? windowHeight : undefined }}>
-      <div>
+      className={styles.Container}>
+      <div className={styles.InnerContainer}>
         <h1 className={styles.HeaderTitle}>
-          {translate({ message: 'Distributed training made', id: 'landing.title' })}
+          {translate({ id: 'landing.title' })}
         </h1>
         <Spacer height={20} />
-        <HeaderTyper
-          className={styles.HeaderTyper}
-          words={toTypeWords}
-          delay={5000}
-          defaultText={toTypeWords[0] || 'simple'}
-        />
-        <Spacer height={50} />
-        <p className={styles.HeaderTitle}>
-          {translate({ message: 'with', id: 'landing.description.part1' })}
-          <span className={styles.SeparatorText}> Colossal-AI </span>
-          {translate({ message: '', id: 'landing.description.part2' })}
+        <p className={styles.DescriptionText}>
+          {translate({ id: 'landing.description' })}
         </p>
         <Spacer height={50} />
         <div className={styles.ButtonContainer}>
@@ -50,15 +39,18 @@ const HeaderView: React.FC<Props> = ({ getStartedRef }) => {
             className={styles.GetStartedButton}
             to={getStartedUrl}
           >
-            {translate({ message: 'GET STARTED', id: 'landing.getStarted' })}
+            {translate({ id: 'landing.getStarted' })}
           </PrimaryButton>
           <GithubButton
             className={styles.GithubButton}
             to={siteConfig.customFields.githubUrl as any}
           />
+          <SlackButton
+              className={styles.SlackButton}
+              to={siteConfig.customFields.slackUrl as any}
+          />
         </div>
       </div>
-      <Rocket className={styles.RocketImage} />
     </div>
   );
 };
